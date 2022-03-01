6348280711c6e1e6214ed8fdb5f6d5f24902dbf7
AbdElrahman <201727156@std.sci.cu.edu.eg>
2022-03-01 17:24:04 +0200

    init repo create base app create home screen

---
+++ android/app/src/main/kotlin/com/maro/islami/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.maro.islami
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:islami/Screens/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
